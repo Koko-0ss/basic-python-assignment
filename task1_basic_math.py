9c54e108acbae973eba5e1a9597a9cd4ce24f610
Koko-0ss <krutikakukde23@gmail.com>
2025-12-30 14:23:29 +0530

    Add files via upload

---
+++ task1_basic_math.py
@@ -0,0 +1,22 @@
+#TASK 1: PERFOR BASIC MATHEMATICAL CALCULATIONS
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Performing operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+
+# Handle division safely
+if num2 != 0:
+    division = num1 / num2
+else:
+    division = "Undefined (division by zero)"
+
+# Displaying results
+print("\nResults:")
+print("Addition: ", addition)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
